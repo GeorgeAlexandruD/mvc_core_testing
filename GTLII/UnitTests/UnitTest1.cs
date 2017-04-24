40cb7d5c5dd142d709b474d3f708a6084be90c27
Draganoiu George <draganoiugeorge@gmail.com>
2017-04-24 22:10:44 +0200

    test project

---
+++ GTLII/UnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GTLII.Controllers;
+using GTLII.Services;
+using Moq;
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        BookController bc;
+        Mock<BooksRepository> repoMock;
+        [TestInitialize]
+        public void BeforeRun()
+        {
+            repoMock = new Mock<BooksRepository>();
+            bc = new BookController(repoMock.Object);
+        }
+        [TestMethod]
+        public void TestGetAllBooks()
+        {
+            //magic shit happening here
+        }
+    }
+}
